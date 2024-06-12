33f753c3b62d35beb1636c81a29546c3b2ed042f
Pape Samba NDOUR <papesambandour@hotmail.com>
2024-06-12 12:13:10 +0000

    before update

--- 1-pre-requis-lite.sh
+++ 1-pre-requis-lite.sh
@@ -2,14 +2,14 @@
 set -a; source .env; set +a
 sudo yum update -y
 
-if [ "$NODE_HAS_MANAGER" -eq 1 ]; then
-    echo "MANAGER"
-   # Run the docker swarm init command and capture the token
-   SWARM_TOKEN=$(docker swarm init --advertise-addr "$IP_MANAGER" 2>&1)
-   SWARM_TOKEN=" TO JOIN RUN COMMAND :  $(echo "$SWARM_TOKEN" | grep -o '.*--token.*')"
-   echo "$SWARM_TOKEN" > "$MANAGER_TOKEN_PATH"
-   echo "Swarm token saved to $MANAGER_TOKEN_PATH"
-else
-    echo "WORKER"
-fi
+#if [ "$NODE_HAS_MANAGER" -eq 1 ]; then
+#    echo "MANAGER"
+#   # Run the docker swarm init command and capture the token
+#   SWARM_TOKEN=$(docker swarm init --advertise-addr "$IP_MANAGER" 2>&1)
+#   SWARM_TOKEN=" TO JOIN RUN COMMAND :  $(echo "$SWARM_TOKEN" | grep -o '.*--token.*')"
+#   echo "$SWARM_TOKEN" > "$MANAGER_TOKEN_PATH"
+#   echo "Swarm token saved to $MANAGER_TOKEN_PATH"
+#else
+#    echo "WORKER"
+#fi
 
